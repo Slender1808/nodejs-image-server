aa0c4e2185a1a803b3fb4c1d7d8cc615c71df065
Slender1808 <50336793+Slender1808@users.noreply.github.com>
2020-05-08 12:25:47 -0300

    0.02

--- server.js
+++ server.js
@@ -1,46 +1,46 @@
 const http2 = require('http2')
 const fs = require('fs')
 const path = require('path')
-let router = require('./routes');
+const routes = require('./routes');
 
 var teste = []
-teste.push({ img: fs.readFileSync(path.resolve(__dirname, 'jpg-t.jpg')) })
-teste.push({ img: fs.readFileSync(path.resolve(__dirname, 'png-t.png')) })
-teste.push({ img: fs.readFileSync(path.resolve(__dirname, 'webp-t.webp')) })
+teste.push({ img: fs.readFileSync(path.resolve(__dirname, 'jpg-t.jpg')), name: 'jpg' })
+teste.push({ img: fs.readFileSync(path.resolve(__dirname, 'png-t.png')), name: 'png' })
+teste.push({ img: fs.readFileSync(path.resolve(__dirname, 'webp-t.webp')), name: 'webp' })
 
 const server = http2.createSecureServer({
-  key: fs.readFileSync('localhost-privkey.pem'),
-  cert: fs.readFileSync('localhost-cert.pem')
+   key: fs.readFileSync('localhost-privkey.pem'),
+   cert: fs.readFileSync('localhost-cert.pem')
 });
 
 server.on('stream', (stream, headers) => {
-  console.log(headers[":path"]) // req
-  if (headers[":path"] == '/png') {
-    // stream is a Duplex
-    stream.respond({
-      'content-type': 'image/jpg',
-      ':status': 200
-    });
-    console.log('PNG---')
-    stream.end(teste[1].img);
-  } else if (headers[":path"] == '/jpg') {
-    stream.respond({
-      'content-type': 'image/jpg',
-      ':status': 200
-    });
-    console.log('JPG---')
-    stream.end(teste[0].img);
-  } else if (headers[":path"] == '/webp') {
-    stream.respond({
-      'content-type': 'image/webp',
-      ':status': 200
-    });
-    console.log('WEBP---')
-    stream.end(teste[2].img);
-  } else {
-    stream.respond({ ':status': 200 });
-    stream.end('some data');
-  }
+   console.log(headers[":path"]) // req
+   if (headers[":path"] == '/png') {
+      // stream is a Duplex
+      stream.respond({
+         'content-type': 'image/jpg',
+         ':status': 200
+      });
+      console.log('PNG---')
+      stream.end(teste[1].img);
+   } else if (headers[":path"] == '/jpg') {
+      stream.respond({
+         'content-type': 'image/jpg',
+         ':status': 200
+      });
+      console.log('JPG---')
+      stream.end(teste[0].img);
+   } else if (headers[":path"] == '/webp') {
+      stream.respond({
+         'content-type': 'image/webp',
+         ':status': 200
+      });
+      console.log('WEBP---')
+      stream.end(teste[2].img);
+   } else {
+      stream.respond({ ':status': 200 });
+      stream.end('some data');
+   }
 });
 
 server.listen(8443);
