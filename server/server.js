437bb1c370825ed399797ab8b84f876a543ede29
Slender1808 <50336793+Slender1808@users.noreply.github.com>
2020-08-16 17:40:17 -0300

    0.19.0

--- server/server.js
+++ server/server.js
@@ -19,67 +19,97 @@ fs.writeFileSync("base.json", data);
 // Carregando imagem de 404
 const result404 = "image not found";
 
-http
-  .createServer((request, response) => {
-    console.log(request.url);
-    let raw;
-    let codHTML = 200;
+// Cluster
+var cluster = require("cluster");
 
-    // Busca pelo arquivo
-    const res = JSONPath.query(tree, `$..[?(@.path == '${request.url}')]`);
+if (cluster.isMaster) {
+  // Use the OS module to check how many core's the current machine has.
+  var cpus = require("os").cpus();
 
-    console.log("-----------");
-    console.log(res);
+  // For each core, create a new worker
+  cpus.forEach(function (cpu) {
+    console.log(cpu);
+    cluster.fork();
+  });
 
-    if (res[0] != undefined) {
-      codHTML = 200;
-      if (res[0].type != "directory") {
-        raw = fs.createReadStream("public" + res[0].path);
+  // Workers will emit an 'online' event when they spawn
+  cluster.on("online", function (worker) {
+    console.log(
+      "Worker " +
+        worker.id +
+        " is here to chew bubblegum and scale node applications."
+    );
+  });
+
+  // Workers will emit an 'exit' event when they exit
+  cluster.on("exit", function (worker, code, signal) {
+    console.log(
+      "Worker " + worker.id + " died with code " + code + ". RIP in peace."
+    );
+    // You can maintain a constant number of workers by forking when a worker exits
+    cluster.fork();
+  });
+} else {
+  http
+    .createServer((request, response) => {
+      console.log(request.url);
+      let raw;
+      let codHTML = 200;
+
+      // Busca pelo arquivo
+      const res = JSONPath.query(tree, `$..[?(@.path == '${request.url}')]`);
+      
+      console.log("Request to worker 🖥  " + cluster.worker.id);
+       
+      if (res[0] != undefined) {
+        codHTML = 200;
+        if (res[0].type != "directory") {
+          raw = fs.createReadStream("public" + res[0].path);
+        } else {
+          raw = JSON.stringify(res);
+        }
       } else {
-        raw = JSON.stringify(res);
+        console.log("404");
+        codHTML = 404;
+        raw = result404;
       }
-    } else {
-      console.log("404");
-      codHTML = 404;
-      raw = result404;
-    }
 
-    // Store both a compressed and an uncompressed version of the resource.
-    response.setHeader("Vary", "Accept-Encoding");
-    //request.setHeader('content-type', res.contentType);
-    let acceptEncoding = request.headers["accept-encoding"];
-    if (!acceptEncoding) {
-      acceptEncoding = "";
-    }
-
-    const onError = (err) => {
-      if (err) {
-        // If an error occurs, there's not much we can do because
-        // the server has already sent the 200 response code and
-        // some amount of data has already been sent to the client.
-        // The best we can do is terminate the response immediately
-        // and log the error.
-        response.end();
-        console.error("An error occurred:", err);
+      // Store both a compressed and an uncompressed version of the resource.
+      response.setHeader("Vary", "Accept-Encoding");
+      //request.setHeader('content-type', res.contentType);
+      let acceptEncoding = request.headers["accept-encoding"];
+      if (!acceptEncoding) {
+        acceptEncoding = "";
       }
-    };
 
-    // Note: This is not a conformant accept-encoding parser.
-    // See https://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.3
-    if (/\bdeflate\b/.test(acceptEncoding)) {
-      response.writeHead(codHTML, { "Content-Encoding": "deflate" });
-      pipeline(raw, zlib.createDeflate(), response, onError);
-    } else if (/\bgzip\b/.test(acceptEncoding)) {
-      response.writeHead(codHTML, { "Content-Encoding": "gzip" });
-      pipeline(raw, zlib.createGzip(), response, onError);
-    } else if (/\bbr\b/.test(acceptEncoding)) {
-      response.writeHead(codHTML, { "Content-Encoding": "br" });
-      pipeline(raw, zlib.createBrotliCompress(), response, onError);
-    } else {
-      response.writeHead(codHTML, {});
-      pipeline(raw, response, onError);
-    }
-  })
-  .listen(80);
+      const onError = (err) => {
+        if (err) {
+          // If an error occurs, there's not much we can do because
+          // the server has already sent the 200 response code and
+          // some amount of data has already been sent to the client.
+          // The best we can do is terminate the response immediately
+          // and log the error.
+          response.end();
+          console.error("An error occurred:", err);
+        }
+      };
 
-console.log("Server On");
+      // Note: This is not a conformant accept-encoding parser.
+      // See https://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.3
+      if (/\bdeflate\b/.test(acceptEncoding)) {
+        response.writeHead(codHTML, { "Content-Encoding": "deflate" });
+        pipeline(raw, zlib.createDeflate(), response, onError);
+      } else if (/\bgzip\b/.test(acceptEncoding)) {
+        response.writeHead(codHTML, { "Content-Encoding": "gzip" });
+        pipeline(raw, zlib.createGzip(), response, onError);
+      } else if (/\bbr\b/.test(acceptEncoding)) {
+        response.writeHead(codHTML, { "Content-Encoding": "br" });
+        pipeline(raw, zlib.createBrotliCompress(), response, onError);
+      } else {
+        response.writeHead(codHTML, {});
+        pipeline(raw, response, onError);
+      }
+    })
+    .listen(80);
+  console.log("Server On 🚀");
+}
