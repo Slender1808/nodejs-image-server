28c9ee0e5f24ad63eef2a51c771c73076e33bb4a
Slender1808 <50336793+Slender1808@users.noreply.github.com>
2020-08-16 13:15:41 -0300

    0.15.0

--- server/routes.js
+++ server/routes.js
@@ -4,14 +4,12 @@ const JSONPath = require('jsonpath')
 module.exports = function routes(headers, dir) {
  
   const result = JSONPath.query(dir, `$..[?(@.path == '${headers.substr(1)}')]`)
-  console.log(result[0].path)
 
   if (result.length > 0) {
     if (result[0].type == 'folder') {
-      result[0].byte = JSON.stringify(result[0])
+      result[0].byte = JSON.stringify(result)
       return result[0]
     } else {
-      result[0].byte = fs.readFileSync(result[0].path)
       return result[0]
     }
   } else {
--- server/server.js
+++ server/server.js
@@ -1,44 +1,83 @@
-const http2 = require("http2");
+const http = require("http");
 const fs = require("fs");
 const JSONPath = require("jsonpath");
+const { pipeline } = require("stream");
+const zlib = require("zlib");
 
 const routes = require("./routes");
-const dirTree = require("../modules/dirTree");
 
-var dir = dirTree("public");
+// Carregando diretorio public
+const dirTree = require("directory-tree");
+const tree = dirTree("public");
 
-let data = JSON.stringify(dir);
+// Salvando JSON do diretorio public
+let data = JSON.stringify(tree);
 fs.writeFileSync("base.json", data);
 
-const server = http2.createSecureServer({
-  key: fs.readFileSync("./localhost-privkey.pem"),
-  cert: fs.readFileSync("./localhost-cert.pem"),
-});
-
+// Carregando imagem de 404
 const result404 = JSONPath.query(
-  dir,
+  tree,
   "$..[?(@.type=='file' && @.path=='public/semfoto.webp')]"
 )[0];
-result404.byte = fs.readFileSync(result404.path);
+result404.byte = fs.createReadStream(result404.path);
 
-server.on("stream", (stream, headers) => {
-  // stream is a Duplex
-  let res = routes(headers[":path"], dir);
-  console.log();
-  if (res != 404) {
-    stream.respond({
-      "content-type": res.contentType,
-      ":status": 200,
-    });
-    stream.end(res.byte);
-  } else {
-    stream.respond({
-      "content-type": result404.contentType,
-      ":status": 404,
-    });
-    stream.end(result404.byte);
-  }
-});
+http
+  .createServer((request, response) => {
+    console.log(request.url);
+    let raw;
+    let res;
 
-server.listen(8443);
+    res = routes(request.url, tree);
+    console.log("-----------");
+    console.log(res);
+    if (res != 404) {
+      if (res.type != "directory") {
+        raw = fs.createReadStream(res.path);
+        //request.setHeader('Content-Type', res.contentType);
+      } else {
+        raw = JSON.stringify(res);
+        //request.setHeader('Content-Type', "application/json");
+      }
+    } else {
+      raw = result404.byte;
+    }
+
+    // Store both a compressed and an uncompressed version of the resource.
+    response.setHeader("Vary", "Accept-Encoding");
+    //request.setHeader('content-type', res.contentType);
+    let acceptEncoding = request.headers["accept-encoding"];
+    if (!acceptEncoding) {
+      acceptEncoding = "";
+    }
+
+    const onError = (err) => {
+      if (err) {
+        // If an error occurs, there's not much we can do because
+        // the server has already sent the 200 response code and
+        // some amount of data has already been sent to the client.
+        // The best we can do is terminate the response immediately
+        // and log the error.
+        response.end();
+        console.error("An error occurred:", err);
+      }
+    };
+
+    // Note: This is not a conformant accept-encoding parser.
+    // See https://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.3
+    if (/\bdeflate\b/.test(acceptEncoding)) {
+      response.writeHead(200, { "Content-Encoding": "deflate" });
+      pipeline(raw, zlib.createDeflate(), response, onError);
+    } else if (/\bgzip\b/.test(acceptEncoding)) {
+      response.writeHead(200, { "Content-Encoding": "gzip" });
+      pipeline(raw, zlib.createGzip(), response, onError);
+    } else if (/\bbr\b/.test(acceptEncoding)) {
+      response.writeHead(200, { "Content-Encoding": "br" });
+      pipeline(raw, zlib.createBrotliCompress(), response, onError);
+    } else {
+      response.writeHead(200, {});
+      pipeline(raw, response, onError);
+    }
+  })
+  .listen(80);
+
 console.log("Server On");
